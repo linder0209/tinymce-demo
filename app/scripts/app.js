04fed73dfcae64f7e857d2ed70dbfd34cd21e849
linder0209 <linder0209@126.com>
2015-03-30 11:44:13 +0800

    init the app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tinymceApp
+ * @description
+ * # tinymceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tinymceApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tinymceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tinymceApp
+ */
+angular.module('tinymceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
